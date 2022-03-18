fe39f0e5188ae0f154c6d9aaa8df7e20eec29258
QuadStingray <github@quadstingray.com>
2022-03-18 07:53:42 +0100

    fix: Add id to FileInformation and rename method on buckets for valid openapi specification

--- src/main/scala/com/quadstingray/mongo/camp/model/DBFileInformation.scala
+++ src/main/scala/com/quadstingray/mongo/camp/model/DBFileInformation.scala
@@ -1,6 +1,6 @@
 package com.quadstingray.mongo.camp.model
-import org.mongodb.scala.bson.Document
+import org.mongodb.scala.bson.{ Document, ObjectId }
 
 import java.util.Date
 
-case class DBFileInformation(filename: String, length: Long, chunkSize: Long, uploadDate: Date, metadata: Option[Document])
+case class DBFileInformation(_id: ObjectId, filename: String, length: Long, chunkSize: Long, uploadDate: Date, metadata: Option[Document])
