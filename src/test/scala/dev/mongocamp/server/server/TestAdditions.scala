80cb7fb78f79c3ca6baf806b771ed8ba934ec206
QuadStingray <github@quadstingray.com>
2022-03-31 21:00:38 +0200

    revert: CollectionSuite reverted

--- src/test/scala/dev/mongocamp/server/server/TestAdditions.scala
+++ src/test/scala/dev/mongocamp/server/server/TestAdditions.scala
@@ -6,7 +6,7 @@ import dev.mongocamp.server.auth.{ AuthHolder, MongoAuthHolder }
 import dev.mongocamp.server.converter.CirceSchema
 import dev.mongocamp.server.database.MongoDatabase
 import dev.mongocamp.server.model.auth.{ Grant, Role, UserInformation }
-import io.circe.parser.decode
+import dev.mongocamp.server.service.SystemFileService
 import org.joda.time.DateTime
 import sttp.capabilities
 import sttp.capabilities.akka.AkkaStreams
@@ -34,9 +34,9 @@ object TestAdditions extends CirceSchema {
   case class MapCollectionDao(collectionName: String) extends MongoDAO[Map[String, Any]](MongoDatabase.databaseProvider, collectionName)
 
   def importData(): Boolean = {
-    MapCollectionDao("accounts").insertMany(readJson("/accounts.json")).result()
-    MapCollectionDao("users").insertMany(readJson("/users.json")).result()
-    MapCollectionDao("users").insertMany(readJson("/users.json")).result()
+    MapCollectionDao("accounts").insertMany(SystemFileService.readJsonList("accounts.json")).result()
+    MapCollectionDao("users").insertMany(SystemFileService.readJsonList("users.json")).result()
+    MapCollectionDao("users").insertMany(SystemFileService.readJsonList("users.json")).result()
     val geoDataDao = MapCollectionDao("geodata:locations")
     val geoJson    = readGeoDataJson()
     geoDataDao.insertMany(geoJson).result()
@@ -81,26 +81,20 @@ object TestAdditions extends CirceSchema {
     }
   }
 
-  def readJson(fileName: String): List[Map[String, Any]] = {
-    val file        = File(getClass.getResource(fileName).getPath)
-    val fileContent = file.contentAsString
-    val decoded     = decode[List[Map[String, Any]]](fileContent)
-    val list        = decoded.getOrElse(List())
-    list
-  }
-
   def readGeoDataJson(): List[Map[String, Any]] = {
-    val list = readJson("/geodata.json").map(element => {
-      val newElement = mutable.Map[String, Any]()
-      newElement.put("name", element("name"))
-      val geoData   = element("geodata").toString.split(", ")
-      val longitude = geoData.last.toDouble
-      val latitude  = geoData.head.toDouble
-      newElement.put("geodata", Map("type" -> "Point", "coordinates" -> List(longitude, latitude)))
-      newElement.put("type", "company")
-      newElement.put("checkedAt", new DateTime(element("checkedAt")).toDate)
-      newElement.toMap
-    })
+    val list = SystemFileService
+      .readJsonList("geodata.json")
+      .map(element => {
+        val newElement = mutable.Map[String, Any]()
+        newElement.put("name", element("name"))
+        val geoData   = element("geodata").toString.split(", ")
+        val longitude = geoData.last.toDouble
+        val latitude  = geoData.head.toDouble
+        newElement.put("geodata", Map("type" -> "Point", "coordinates" -> List(longitude, latitude)))
+        newElement.put("type", "company")
+        newElement.put("checkedAt", new DateTime(element("checkedAt")).toDate)
+        newElement.toMap
+      })
     list
   }
 
---
+++ src/test/scala/dev/mongocamp/server/tests/CollectionSuite.scala
@@ -0,0 +1,150 @@
+package dev.mongocamp.server.tests
+
+import com.sfxcode.nosql.mongo._
+import dev.mongocamp.server.client.api.{ CollectionApi, DatabaseApi }
+import dev.mongocamp.server.client.model.{ MongoAggregateRequest, PipelineStage }
+import dev.mongocamp.server.database.MongoDatabase
+
+class CollectionSuite extends BaseSuite {
+
+  val collectionApi: CollectionApi = CollectionApi()
+  val databaseApi: DatabaseApi     = DatabaseApi()
+
+  test("list all collections as admin") {
+    val response = executeRequestToResponse(collectionApi.listCollections("", adminBearerToken)())
+    assertEquals(
+      response,
+      List(
+        "accounts",
+        "admin-test",
+        "mc_request_logging",
+        "mc_roles",
+        "mc_token_cache",
+        "mc_users",
+        "sample-files.chunks",
+        "sample-files.files",
+        "test",
+        "users"
+      )
+    )
+    assertEquals(response.size, 10)
+
+  }
+
+  test("collection status accounts as admin") {
+    val response = executeRequestToResponse(collectionApi.getCollectionInformation("", adminBearerToken)("accounts"))
+    assertEquals(response.size, 10105.0)
+    assertEquals(response.count, 100)
+  }
+
+  test("delete collection infos as admin") {
+    MongoDatabase.databaseProvider.dao("deleteTest").createIndexForField("index_for_test").result()
+    val response = executeRequestToResponse(collectionApi.getCollectionInformation("", adminBearerToken)("deleteTest"))
+    assertEquals(response.ns, "test.deleteTest")
+    val deleteResponse = executeRequestToResponse(collectionApi.deleteCollection("", adminBearerToken)("deleteTest"))
+    assertEquals(deleteResponse.value, true)
+  }
+
+  test("clear collection infos as admin") {
+    val collection = MongoDatabase.databaseProvider.dao("otherDB:collectionName")
+    collection.insertOne(Map("key" -> "value")).result()
+    assertEquals(collection.count().result(), 1L)
+    val deleteResponseResult = executeRequestToResponse(collectionApi.clearCollection("", adminBearerToken)("otherDB:collectionName"))
+    assertEquals(deleteResponseResult.value, true)
+    assertEquals(collection.count().result(), 0L)
+    val databaseDelete = executeRequestToResponse(databaseApi.deleteDatabase("", adminBearerToken)("otherDB"))
+    assertEquals(databaseDelete.value, true)
+  }
+
+  test("distinct on collection as admin") {
+    val distinct = executeRequestToResponse(collectionApi.distinct("", adminBearerToken)("users", "numberrange"))
+    assertEquals(distinct.size, 11)
+    assertEquals(distinct.sortBy(f => f.toString.toLong), List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+  }
+
+  test("aggregation on collection as admin") {
+    val geoNearMap: Map[String, Any] = Map(
+      "near"          -> Map("type" -> "Point", "coordinates" -> List(-113.4483806208, 44.0010717100)),
+      "distanceField" -> "dist.calculated",
+      "maxDistance"   -> 200,
+      "query"         -> Map("type" -> "company"),
+      "includeLocs"   -> "dist.location",
+      "spherical"     -> true
+    )
+    val aggregateRequest = MongoAggregateRequest(
+      List(
+        PipelineStage("geoNear", geoNearMap)
+      ),
+      allowDiskUse = true
+    )
+    val aggreationResult = executeRequestToResponse(collectionApi.aggregate("", adminBearerToken)("geodata:locations", aggregateRequest))
+    assertEquals(aggreationResult.size, 1)
+    val headResponse = aggreationResult.head
+    assertEquals(headResponse("name").toString, "Mollis Dui Associates")
+    assertEquals(headResponse("type").toString, "company")
+  }
+
+  test("fields on collection as admin") {
+    val distinct = executeRequestToResponse(collectionApi.getCollectionFields("", adminBearerToken)("geodata:locations"))
+    assertEquals(distinct.size, 5)
+    assertEquals(distinct.sorted, List("_id", "checkedAt", "geodata", "name", "type"))
+  }
+
+  test("list all collections as user") {
+    val response = executeRequestToResponse(collectionApi.listCollections("", testUserBearerToken)())
+    assertEquals(response.size, 3)
+    assertEquals(response, List("accounts", "test", "users"))
+  }
+
+  test("collection status accounts as user") {
+    val response = executeRequestToResponse(collectionApi.getCollectionInformation("", testUserBearerToken)("accounts"))
+    assertEquals(response.size, 10105.0)
+    assertEquals(response.count, 100)
+  }
+
+  test("collection status companies as user") {
+    val responseResult = executeRequest(collectionApi.getCollectionInformation("", testUserBearerToken)("companies"))
+    assertEquals(responseResult.code.code, 401)
+    assertEquals(responseResult.header("x-error-message").isDefined, true)
+    assertEquals(responseResult.header("x-error-message").get, "user not authorized for collection")
+  }
+
+  test("distinct on collection as user") {
+    val distinct = executeRequestToResponse(collectionApi.distinct("", testUserBearerToken)("users", "numberrange"))
+    assertEquals(distinct.size, 11)
+    assertEquals(distinct.sortBy(f => f.toString.toLong), List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+  }
+
+  test("delete collection infos as user") {
+    MongoDatabase.databaseProvider.dao("deleteTest").createIndexForField("index_for_test").result()
+    val response = executeRequestToResponse(collectionApi.getCollectionInformation("", testUserBearerToken)("deleteTest"))
+    assertEquals(response.ns, "test.deleteTest")
+    val deleteResponseResult = executeRequest(collectionApi.deleteCollection("", testUserBearerToken)("deleteTest"))
+    assertEquals(deleteResponseResult.code.code, 401)
+    assertEquals(deleteResponseResult.header("x-error-message").isDefined, true)
+    assertEquals(deleteResponseResult.header("x-error-message").get, "user not authorized for collection")
+  }
+
+  test("aggregation on collection as user") {
+    val geoNearMap: Map[String, Any] = Map(
+      "near"          -> Map("type" -> "Point", "coordinates" -> List(-113.4483806208, 44.0010717100)),
+      "distanceField" -> "dist.calculated",
+      "maxDistance"   -> 200,
+      "query"         -> Map("type" -> "company"),
+      "includeLocs"   -> "dist.location",
+      "spherical"     -> true
+    )
+    val aggregateRequest = MongoAggregateRequest(
+      List(
+        PipelineStage("geoNear", geoNearMap)
+      ),
+      allowDiskUse = true
+    )
+    val aggreationResult = executeRequestToResponse(collectionApi.aggregate("", testUserBearerToken)("geodata:locations", aggregateRequest))
+    assertEquals(aggreationResult.size, 1)
+    val headResponse = aggreationResult.head
+    assertEquals(headResponse("name").toString, "Mollis Dui Associates")
+    assertEquals(headResponse("type").toString, "company")
+  }
+
+}
