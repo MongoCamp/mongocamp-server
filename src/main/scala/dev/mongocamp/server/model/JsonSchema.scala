dce548f024fb355a59d837b513b408657139015e
QuadStingray <github@quadstingray.com>
2022-05-02 08:52:10 +0200

    fix: Type and some other small errors at schema generation

--- src/main/scala/dev/mongocamp/server/model/JsonSchema.scala
+++ src/main/scala/dev/mongocamp/server/model/JsonSchema.scala
@@ -4,6 +4,6 @@ case class JsonSchema(`$schema`: String, `$ref`: String, definitions: Map[String
 
 object JsonSchema {
   def apply(objectName: String, definitions: Map[String, JsonSchemaDefinition]): JsonSchema = {
-    JsonSchema("http://json-schema.org/draft-09/schema#", s"#/definitions/$objectName", definitions)
+    JsonSchema("https://json-schema.org/draft/2020-12/schema", s"#/definitions/$objectName", definitions)
   }
 }
