d14363c6cca131df46fca4fa477e1c29cc9f3d55
QuadStingray <github@quadstingray.com>
2023-01-18 22:04:56 +0100

    feat: MongoDb Micrometer Registry

---
+++ src/main/scala/dev/mongocamp/micrometer/mongodb/registry/MonitoringMongoConfig.scala
@@ -0,0 +1,28 @@
+package dev.mongocamp.micrometer.mongodb.registry
+
+import better.files.File
+import com.typesafe.config.{Config, ConfigFactory}
+import dev.mongocamp.driver.mongodb.MongoDAO
+import io.micrometer.core.instrument.step.StepRegistryConfig
+import org.mongodb.scala.Document
+
+case class MonitoringMongoConfig(mongoDAO: MongoDAO[Document], configurationMap: Map[String, String] = Map()) extends StepRegistryConfig {
+
+  private lazy val conf: Config = ConfigFactory.load()
+
+  override def prefix(): String = "dev.mongocamp.micrometer.mongodb"
+
+  override def get(key: String): String = {
+    try {
+      val mapKey = key.replace(s"$prefix().", "")
+      (configurationMap.get(mapKey) ++ configurationMap.get(key)).foreach(v => {
+        return v
+      })
+      conf.getValue(key).render().replace("\"", "")
+    } catch {
+      case e: Exception =>
+        null
+    }
+  }
+
+}
