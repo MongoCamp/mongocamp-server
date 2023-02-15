8a1bc9216de9a9a9bd10e6b3aedca6e1f9f967a4
QuadStingray <github@quadstingray.com>
2023-02-15 21:49:30 +0100

    refactor(metrics): Micrometer MongoDb Package

--- src/main/scala/dev/mongocamp/plugins/monitoring/mongodb/MongoDbMetricsLoggingPlugin.scala
+++ src/main/scala/dev/mongocamp/plugins/monitoring/mongodb/MongoDbMetricsLoggingPlugin.scala
@@ -10,33 +10,33 @@ import dev.mongocamp.server.service.ConfigurationService
 
 import scala.concurrent.duration.Duration
 
-object MongoDbMetricsPlugin extends ServerPlugin with LazyLogging {
-  private val ConfKeyMicrometerStep = "micrometer.mongodb.step"
-  private val ConfKeyMonitorJVM     = "micrometer.mongodb.jvm"
-  private val ConfKeyMonitorSystem  = "micrometer.mongodb.system"
-  private val ConfKeyMonitorMongo   = "micrometer.mongodb.mongo"
-  private val ConfKeyMonitorEvent   = "micrometer.mongodb.event"
+object MongoDbMetricsLoggingPlugin extends ServerPlugin with LazyLogging {
+  private val ConfKeyMicrometerStep         = "micrometer.mongodb.step"
+  private val ConfKeyLoggingJvmToMongoDb    = "micrometer.mongodb.jvm"
+  private val ConfKeyLoggingSystemToMongoDb = "micrometer.mongodb.system"
+  private val ConfKeyLoggingMongoToMongoDb  = "micrometer.mongodb.mongo"
+  private val ConfKeyLoggingEventToMongoDb  = "micrometer.mongodb.event"
 
   override def activate(): Unit = {
 
-    ConfigurationService.registerConfig(ConfKeyMonitorJVM, MongoCampConfiguration.confTypeBoolean)
-    ConfigurationService.registerConfig(ConfKeyMonitorSystem, MongoCampConfiguration.confTypeBoolean)
-    ConfigurationService.registerConfig(ConfKeyMonitorMongo, MongoCampConfiguration.confTypeBoolean)
-    ConfigurationService.registerConfig(ConfKeyMonitorEvent, MongoCampConfiguration.confTypeBoolean)
     ConfigurationService.registerConfig(ConfKeyMicrometerStep, MongoCampConfiguration.confTypeDuration)
+    ConfigurationService.registerConfig(ConfKeyLoggingJvmToMongoDb, MongoCampConfiguration.confTypeBoolean)
+    ConfigurationService.registerConfig(ConfKeyLoggingSystemToMongoDb, MongoCampConfiguration.confTypeBoolean)
+    ConfigurationService.registerConfig(ConfKeyLoggingMongoToMongoDb, MongoCampConfiguration.confTypeBoolean)
+    ConfigurationService.registerConfig(ConfKeyLoggingEventToMongoDb, MongoCampConfiguration.confTypeBoolean)
 
     val step      = java.time.Duration.ofSeconds(ConfigurationService.getConfigValue[Duration](ConfKeyMicrometerStep).toSeconds)
     val configMap = Map("step" -> step.toString)
-    if (ConfigurationService.getConfigValue[Boolean](ConfKeyMonitorJVM)) {
+    if (ConfigurationService.getConfigValue[Boolean](ConfKeyLoggingJvmToMongoDb)) {
       MetricsConfiguration.addJvmRegistry(MongoStepMeterRegistry(MongoDatabase.databaseProvider.dao("monitoring_jvm"), configMap))
     }
-    if (ConfigurationService.getConfigValue[Boolean](ConfKeyMonitorSystem)) {
+    if (ConfigurationService.getConfigValue[Boolean](ConfKeyLoggingSystemToMongoDb)) {
       MetricsConfiguration.addSystemRegistry(MongoStepMeterRegistry(MongoDatabase.databaseProvider.dao("monitoring_system"), configMap))
     }
-    if (ConfigurationService.getConfigValue[Boolean](ConfKeyMonitorMongo)) {
+    if (ConfigurationService.getConfigValue[Boolean](ConfKeyLoggingMongoToMongoDb)) {
       MetricsConfiguration.addMongoRegistry(MongoStepMeterRegistry(MongoDatabase.databaseProvider.dao("monitoring_mongo_db"), configMap))
     }
-    if (ConfigurationService.getConfigValue[Boolean](ConfKeyMonitorEvent)) {
+    if (ConfigurationService.getConfigValue[Boolean](ConfKeyLoggingEventToMongoDb)) {
       MetricsConfiguration.addEventRegistry(MongoStepMeterRegistry(MongoDatabase.databaseProvider.dao("monitoring_event"), configMap))
     }
   }
