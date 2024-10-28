bf0af675540585b3777ee96c728e0b906ec458c1
QuadStingray <git@quadstingray.dev>
2024-10-28 21:45:45 +0100

    revert: RequestLogging only if node is coordinator

--- mongocamp-plugin-requestlogging/src/main/scala/dev/mongocamp/server/plugin/requestlogging/listener/RequestLoggingActor.scala
+++ mongocamp-plugin-requestlogging/src/main/scala/dev/mongocamp/server/plugin/requestlogging/listener/RequestLoggingActor.scala
@@ -3,7 +3,8 @@ package dev.mongocamp.server.plugin.requestlogging.listener
 import com.typesafe.scalalogging.LazyLogging
 import dev.mongocamp.driver.mongodb._
 import dev.mongocamp.server.BuildInfo
-import dev.mongocamp.server.event.http.{ HttpRequestCompletedEvent, HttpRequestStartEvent }
+import dev.mongocamp.server.event.EventSystem
+import dev.mongocamp.server.event.http.{HttpRequestCompletedEvent, HttpRequestStartEvent}
 import dev.mongocamp.server.plugin.requestlogging.database.RequestLoggingDatabase
 import org.apache.pekko.actor.Actor
 
@@ -16,30 +17,34 @@ private[plugin] class RequestLoggingActor extends Actor with LazyLogging {
       logger.info(this.getClass.getSimpleName)
 
     case event: HttpRequestCompletedEvent =>
-      val updateMap      = Map("$set" -> Map("duration" -> event.duration.getMillis, "responseCode" -> event.responseCode))
-      val updateResponse = RequestLoggingDatabase.requestLoggingDao.updateOne(Map("requestId" -> event.requestId), updateMap).result()
-      updateResponse
+      if (EventSystem.isCoordinator) {
+        val updateMap      = Map("$set" -> Map("duration" -> event.duration.getMillis, "responseCode" -> event.responseCode))
+        val updateResponse = RequestLoggingDatabase.requestLoggingDao.updateOne(Map("requestId" -> event.requestId), updateMap).result()
+        updateResponse
+      }
 
     case event: HttpRequestStartEvent =>
-      val requestLogging = DatabaseRequestLoggingElement(
-        event.eventDate,
-        BuildInfo.name,
-        BuildInfo.version,
-        InetAddress.getLocalHost.toString,
-        event.requestId,
-        event.httpMethod,
-        event.methodName,
-        event.uri,
-        event.remoteAddress,
-        event.userId,
-        -1,
-        -1,
-        event.controller,
-        event.controllerMethod,
-        event.comment
-      )
-      val insertResponse = RequestLoggingDatabase.requestLoggingDao.insertOne(requestLogging).result()
-      insertResponse
+      if (EventSystem.isCoordinator) {
+        val requestLogging = DatabaseRequestLoggingElement(
+          event.eventDate,
+          BuildInfo.name,
+          BuildInfo.version,
+          InetAddress.getLocalHost.toString,
+          event.requestId,
+          event.httpMethod,
+          event.methodName,
+          event.uri,
+          event.remoteAddress,
+          event.userId,
+          -1,
+          -1,
+          event.controller,
+          event.controllerMethod,
+          event.comment
+        )
+        val insertResponse = RequestLoggingDatabase.requestLoggingDao.insertOne(requestLogging).result()
+        insertResponse
+      }
   }
 
 }
